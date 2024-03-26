b5e854c71becfe94705c21e5176a2f864ad3e267
Alexandra <pitarualexandra@yahoo.com>
2024-03-26 13:22:47 +0200

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.pt2024_30423_pitaru_alexandra_assignment_2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example to javafx.fxml;
+    exports org.example;
+}
---
+++ src/main/java/org/example/Model/Server.java
@@ -0,0 +1,24 @@
+package org.example.Model;
+
+public class Server implements Runnable{
+    public Server()
+    {
+
+    }
+    public void addTask(Task newTask)
+    {
+
+    }
+    @Override
+    public void run() {
+     while(true)
+     {
+
+     }
+    }
+    public Task[] getTasks()
+    {
+
+        return new Task[0];
+    }
+}
